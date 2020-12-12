3dd75d0d5ac00aba12e30b8046e96841dcfb564a
VsevolodJScript <VsevolodGitHub@users.noreply.github.com>
2020-12-12 22:11:32 +1000

    Clicker Update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(function () {
     let score = 0
     $("#plus").on("click", function () {
         score = score + 1
-        $(".result").remove()
-        $("body").append('<div class="result">')
-        $(".result").append("Счёт: " + score)
+        $("h3").remove()
+        $("body").append('<h3>')
+        $("h3").append("Score: " + score)
     })
 })
